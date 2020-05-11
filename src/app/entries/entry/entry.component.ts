95bfae9df8abde0141d85358bb806738ce19e4e3
GoodGuyGregory <greg.witt625@gmail.com>
2020-05-11 14:23:36 -0400

    adds functionality to entry-component

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -12,4 +12,10 @@ export class EntryComponent {
     title: string = 'My first photo'
     photo: string = 'http://placehold.it/800x500?text=AngularBasics';
     description: string = 'A Description of first photo';
+    comments: any[] = [
+        { name: "John ", comment: "A Comment" },
+        { name: "Jacob ", comment: "A Comment" },
+        { name: "Jingle ", comment: "A Comment" },
+        { name: "Heimenshmit ", comment: "A Comment" },
+    ]
 }
