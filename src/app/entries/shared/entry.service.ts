d079a410204d8579aa7b7868c5962d6e1e694c3f
GoodGuyGregory <greg.witt625@gmail.com>
2020-05-11 16:24:02 -0400

    adds annotation to classes

--- src/app/entries/shared/entry.service.ts
+++ src/app/entries/shared/entry.service.ts
@@ -1,4 +1,6 @@
 import { Entry } from './entry.model';
+// allows for the injectable decoration to dictate it will be doing 
+// dependency injection for app as a provider
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
@@ -10,6 +12,7 @@ export class EntryService {
     }
 
     getEntries(): Promise<Entry[]> {
+        // grabs the local put requests and retrives them over the Http module
         return this.http.get('/app/entries')
             .toPromise()
             .then(response => response.json().data as Entry[]);
